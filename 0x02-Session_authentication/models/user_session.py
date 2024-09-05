5941d68250f83e19dca362df24a2ed8ca2bccbeb
Aymen <aymenabduseid10@gmail.com>
2024-09-05 20:58:16 +0300

    init

--- 0x02-Session_authentication/models/user_session.py
+++ 0x02-Session_authentication/models/user_session.py
@@ -1,16 +1,16 @@
 #!/usr/bin/env python3
-""" User_session module
 """
+UserSession module
+"""
+
 from models.base import Base
 
 
 class UserSession(Base):
-    """ User Session class
-    """
+    """ UserSession class """
 
     def __init__(self, *args: list, **kwargs: dict):
-        """ Initialize a User instance
-        """
+        """ Constructor """
         super().__init__(*args, **kwargs)
         self.user_id = kwargs.get('user_id')
         self.session_id = kwargs.get('session_id')
